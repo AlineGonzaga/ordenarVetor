937016d533ad6bcbc75a56dfad3e83f3ad7be619
Aline Gonzaga <aline22200@gmail.com>
2016-01-20 02:46:53 -0300

    Complementação do ordenamento de vetor.

--- src/vetores/OrdenarVetor.java
+++ src/vetores/OrdenarVetor.java
@@ -12,5 +12,18 @@ public class OrdenarVetor {
 			System.out.println(" Digite um valor: ");
 			vetor[i] = sc.nextInt();
 		}
+		
+		for( int i = 0; i < 3; i++){
+			for(int j = i + 1; j < 4; j++ ){
+				if(vetor[i] > vetor[j]){
+					int aux = vetor[i];
+					vetor[i] = vetor[j];
+					vetor[j] = aux;
+				}
+			}
+		}
+	    for(int i = 0; i < vetor.length; i++){
+	    	System.out.println(vetor[i]);
+	    }
 	}
 }
